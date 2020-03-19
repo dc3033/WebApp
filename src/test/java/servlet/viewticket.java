3c0048727fc04be1be8c248f1cfb6cc0751b2950
dc3033 <dc3033@nyu.edu>
2020-03-18 20:47:22 -0400

    ip change

--- src/test/java/servlet/viewticket.java
+++ src/test/java/servlet/viewticket.java
@@ -16,7 +16,7 @@ public class viewticket extends HttpServlet {
 		try
 		{
 			Class.forName("org.postgresql.Driver");
-			String jdbcUrl="jdbc:postgresql:/3.21.34.231:5432/postgres";
+			String jdbcUrl="jdbc:postgresql:/18.216.123.107:5432/postgres";
 			String username="postgres";
 			String password="";
 						
